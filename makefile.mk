efe0026d236253f97ee90aafdfb31a7bd544f365
TamblesMambles <jrowell5412@gmail.com>
2025-03-11 14:54:01 -0400

    Pray this works makefile.mk

--- makefile.mk
+++ makefile.mk
@@ -16,7 +16,7 @@ WUT_ENABLE_CPP      := 0
 WUT_DEFAULT_MALLOC  := 1
 
 # Target filename
-TARGET              := screenstreaming.mod
+TARGET              := screenstreaming.wps
 
 # Source directories
 SOURCES             := src/
